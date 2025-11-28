803d17bb0b9a4142ac1e7f9d347ad6548d5d5496
hkr-web <snkvtl@gmail.com>
2025-11-28 11:44:52 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function () {
+  
+  $('a[href^="#"]').click(function () {
+    let elmHash = $(this).attr('href');
+    let pos = $(elmHash).offset().top - 110;
+    $("body,html").animate({ scrollTop: pos }, 500);
+    return false;
+  });
+  
+  $(window).scroll(function () {
+    $(".title, .read-more, .concept-text, .left, .center, .right, .interview-left p, .interview-photo1, .interview-photo2, .contact-right p").each(function () {
+      let scroll = $(window).scrollTop();
+      let target = $(this).offset().top;
+      let windowHeight = $(window).height();
+      if (scroll > target - windowHeight + 200 ) {
+        $(this).addClass("slide");
+      }
+    });
+  });
+
+  $(window).scroll(function () {
+    $(".interview-photo3").each(function () {
+      let scroll = $(window).scrollTop();
+      let target = $(this).offset().top;
+      let windowHeight = $(window).height();
+      if (scroll > target - windowHeight + 200) {
+        $(this).addClass("slide-photo3");
+      }
+    });
+  });
+
+  $(window).scroll(function () {
+    $(".contact-left img").each(function () {
+      let scroll = $(window).scrollTop();
+      let target = $(this).offset().top;
+      let windowHeight = $(window).height();
+      if (scroll > target - windowHeight + $(this).outerHeight()) {
+        $(this).addClass("zoomin");
+      }
+    });
+  });
+
+$(".slide-photos").slick({
+    arrows: false,
+    centerMode: true,
+    centerPadding: "100px",
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 1210,
+        settings: {
+          centerPadding: "50px",
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+});
